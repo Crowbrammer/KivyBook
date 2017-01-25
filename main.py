b7c1dc70099a46da1f645fc36c71b929024bb256
Aaron Bell <aaron@crowbrammer.com>
2017-01-25 16:17:50 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+class WeatherApp(App):
+	pass
+
+if __name__ == "__main__":
+	WeatherApp().run()
---
+++ readme.md
@@ -0,0 +1,1 @@
+KivyBook stuff.
