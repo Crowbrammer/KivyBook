bb3b9b461bc3f73ef2640d5ba39017c0b4178dae
Aaron Bell <aaron@crowbrammer.com>
2017-01-29 10:15:20 -0500

    Last non-hail-mary debug

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ class AddLocationForm(BoxLayout):
 	def search_location(self):
 		search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like&APPID=" + "e2c59545ea674f80ab4ce01138ef7b58"
 		search_url = search_template.format(self.search_input.text)
-		
-		request = UrlRequest('http://api.openweathermap.org/data/2.5/weather?lat=34&lon=139&APPID=e2c59545ea674f80ab4ce01138ef7b58', self.found_location)
+		request = UrlRequest(search_url, self.found_location)
 
 	def found_location(self, request, data):
 		data = json.loads(data.decode()) if not isinstance(data, dict) else data
-		print(data)
 		cities = ["{} ({})".format(d['name'], d['sys']['country'])
 			for d in data['list']]
 		self.search_results.item_strings = cities
@@ -26,8 +24,13 @@ class AddLocationForm(BoxLayout):
 		current_location_template = "api.openweathermap.org/data/2.5/weather?lat={}&lon={}&APPID=e2c59545ea674f80ab4ce01138ef7b58"
 		coordinates = self.search_input.text.split(',') #Needs to remove spaces
 		current_location_url = current_location_template.format(coordinates[0], coordinates[1])
-		request = UrlRequest(current_location_url, self.found_location)
-		
+		print(current_location_url)
+		request = UrlRequest(current_location_url, self.processing_stuff)
+		print(request)
+		self.processing_stuff
+	def processing_stuff(self, request, data):		
+		data = json.loads(data.decode()) if not isinstance(data, dict) else data
+		print(data)
 
 class WeatherApp(App):
 	pass
