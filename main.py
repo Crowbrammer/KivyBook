9cb66f515cb6400e51d1eeeafa370537edf0495c
Aaron Bell <aaron@crowbrammer.com>
2017-01-29 09:56:07 -0500

    First commit

--- main.py
+++ main.py
@@ -10,14 +10,24 @@ class AddLocationForm(BoxLayout):
 	def search_location(self):
 		search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like&APPID=" + "e2c59545ea674f80ab4ce01138ef7b58"
 		search_url = search_template.format(self.search_input.text)
-		request = UrlRequest(search_url, self.found_location)
+		
+		request = UrlRequest('http://api.openweathermap.org/data/2.5/weather?lat=34&lon=139&APPID=e2c59545ea674f80ab4ce01138ef7b58', self.found_location)
 
 	def found_location(self, request, data):
 		data = json.loads(data.decode()) if not isinstance(data, dict) else data
+		print(data)
 		cities = ["{} ({})".format(d['name'], d['sys']['country'])
 			for d in data['list']]
 		self.search_results.item_strings = cities
 		print("\n".join(cities))
+		
+		
+	def current_location(self):
+		current_location_template = "api.openweathermap.org/data/2.5/weather?lat={}&lon={}&APPID=e2c59545ea674f80ab4ce01138ef7b58"
+		coordinates = self.search_input.text.split(',') #Needs to remove spaces
+		current_location_url = current_location_template.format(coordinates[0], coordinates[1])
+		request = UrlRequest(current_location_url, self.found_location)
+		
 
 class WeatherApp(App):
 	pass
